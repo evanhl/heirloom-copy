a8892a06f69db415972017687af8b149ec89046e
Evan Owski <evan@heirloom.io>
2014-05-13 17:40:03 -0700

    Fixes picturefill dependency. Changes index.html indentation to 2 spaces. Adds Ember App init in main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,1 @@
-(function () {
-  var $h = $('.hero-unit ul');
-
-  if ($ && $.fn && $.fn.jquery) {
-    $h.append($('<li/>').html('jQuery'));
-  }
-
-  if (Ember && Ember.VERSION) {
-    $h.append($('<li/>').html('Ember'));
-  }
-
-  if (Handlebars && Handlebars.VERSION) {
-    $h.append($('<li/>').html('Handlebars'));
-  }
-})();
+var App = Ember.Application.create({});
