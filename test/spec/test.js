60d6a17ff2d9d1fb083dccce07897305d8ed8049
Evan Owski <evan@heirloom.io>
2014-06-30 23:39:33 -0700

    Adds sign up tests.

--- test/spec/test.js
+++ test/spec/test.js
@@ -20,7 +20,6 @@
   });
 
   describe('Sign In', function () {
-
     after(function () {
       App.reset();
       $.mockjaxClear();
@@ -140,6 +139,91 @@
           });
         });
       });
+    });
+
+    describe('Sign Up', function () {
+      before(function () {
+        visit('registration');
+
+        $.mockjax({
+          url: 'https://api.hlstage.com/registration',
+          type: 'post',
+          data: JSON.stringify({
+            name: 'Big Bird',
+            username: 'bigbird123',
+            email: 'abc@example.com',
+            password: 'b1gb1rd'
+          }),
+          responseText: {
+            name: 'Big Bird',
+            email: 'abc@example.com',
+            username: 'bigbird123',
+            password: 'b1gb1rd',
+            authentication_token: 'tuoo6XoHzTR3Npzp8xRw'
+          }
+        });
+
+        $.mockjax({
+          url: 'https://api.hlstage.com/me/photos',
+          responseText: []
+        });
+
+        fillIn('.name', 'Big Bird');
+        fillIn('.email', 'abc@example.com');
+        fillIn('.username', 'bigbird123');
+        fillIn('.password', 'b1gb1rd');
+        click('button.signup');
+      });
+
+      it('is logged in', function () {
+        andThen(function () {
+          expect(App.get('auth.isLoggedIn')).to.be.true;
+        });
+      });
+
+      it('has the right auth token', function () {
+        andThen(function () {
+          expect(App.get('auth.authToken')).to.equal('tuoo6XoHzTR3Npzp8xRw');
+        });
+      });
+
+      it('has the right session', function () {
+        andThen(function () {
+          expect(App.get('auth.currentSession.name')).to.equal('Big Bird');
+        });
+      });
+
+      it('stores auth token and name in localStorage', function () {
+        andThen(function () {
+          var session = JSON.parse(localStorage.getItem('currentSession'));
+
+          expect(session.authentication_token).to.equal('tuoo6XoHzTR3Npzp8xRw');
+          expect(session.name).to.equal('Big Bird');
+        });
+      });
+
+      it('sign in and sign up link are not rendered', function () {
+        andThen(function () {
+          expect(find('nav .signin')[0]).to.not.exist;
+          expect(find('nav .signup')[0]).to.not.exist;
+        });
+      });
+
+      it('photos, albums, upload link are rendered', function () {
+        andThen(function () {
+          expect(find('nav .photos')[0]).to.exist;
+          expect(find('nav .albums')[0]).to.exist;
+          expect(find('nav .upload')[0]).to.exist;
+          expect(find('nav .signout')[0]).to.exist;
+        });
+      });
+
+      it('user\'s full name is rendered', function () {
+        andThen(function () {
+          expect(find('nav .userfullname').text()).to.equal('Big Bird');
+        });
+      });
+
     });
   });
 })();
