a5d93ccf928ccc556b0b44543475cc8f782808d4
Evan Owski <evan@heirloom.io>
2014-06-30 23:24:13 -0700

    Adds more sign in/out tests.

--- test/spec/test.js
+++ test/spec/test.js
@@ -20,11 +20,9 @@
   });
 
   describe('Sign In', function () {
-    afterEach(function () {
-      App.reset();
-    });
 
     after(function () {
+      App.reset();
       $.mockjaxClear();
     });
 
@@ -51,7 +49,7 @@
 
         fillIn('.login', 'foo');
         fillIn('.password', 'bar');
-        click('.signin');
+        click('button.signin');
       });
 
       it('is logged in', function () {
@@ -69,6 +67,77 @@
       it('has the right session', function () {
         andThen(function () {
           expect(App.get('auth.currentSession.name')).to.equal('Alf Wintheiser Sr.');
+        });
+      });
+
+      it('stores auth token and name in localStorage', function () {
+        andThen(function () {
+          var session = JSON.parse(localStorage.getItem('currentSession'));
+
+          expect(session.authentication_token).to.equal('tuoo6XoHzTR3Npzp8xRw');
+          expect(session.name).to.equal('Alf Wintheiser Sr.');
+        });
+      });
+
+      it('sign in and sign up link are not rendered', function () {
+        andThen(function () {
+          expect(find('nav .signin')[0]).to.not.exist;
+          expect(find('nav .signup')[0]).to.not.exist;
+        });
+      });
+
+      it('photos, albums, upload link are rendered', function () {
+        andThen(function () {
+          expect(find('nav .photos')[0]).to.exist;
+          expect(find('nav .albums')[0]).to.exist;
+          expect(find('nav .upload')[0]).to.exist;
+          expect(find('nav .signout')[0]).to.exist;
+        });
+      });
+
+      it('user\'s full name is rendered', function () {
+        andThen(function () {
+          expect(find('nav .userfullname').text()).to.equal('Alf Wintheiser Sr.');
+        });
+      });
+
+      describe('and subsequent sign out', function () {
+        before(function () {
+          $.mockjax({
+            type: 'delete',
+            url: 'https://api.hlstage.com/session',
+            responseText: null,
+            status: 204
+          });
+
+          click('nav .signout a');
+        });
+
+        it('is logged out', function () {
+          andThen(function () {
+            expect(App.get('auth.isLoggedIn')).to.be.false;
+          });
+        });
+
+        it('clears session from localStorage', function () {
+          andThen(function () {
+            expect(localStorage.getItem('currentSession')).to.be.null;
+          });
+        });
+
+        it('sign in and sign up link are rendered', function () {
+          andThen(function () {
+            expect(find('nav .signin')[0]).to.exist;
+            expect(find('nav .signup')[0]).to.exist;
+          });
+        });
+
+        it('photos, albums, upload link are not rendered', function () {
+          andThen(function () {
+            expect(find('nav .photos')[0]).to.not.exist;
+            expect(find('nav .albums')[0]).to.not.exist;
+            expect(find('nav .upload')[0]).to.not.exist;
+          });
         });
       });
     });
