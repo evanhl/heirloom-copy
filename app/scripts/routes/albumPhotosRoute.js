0801b09963007a7dfd827a36f32f3b0772259878
Evan Owski <evan@heirloom.io>
2014-07-05 19:14:10 -0700

    Adds missing dependency

--- app/scripts/routes/albumPhotosRoute.js
+++ app/scripts/routes/albumPhotosRoute.js
@@ -1,3 +1,4 @@
 //= require signedInRouteMixin
+//= require resetableRouteMixin
 //= require ../utils/infiniteScroll
 App.AlbumPhotosRoute = Ember.Route.extend(App.SignedInRouteMixin, InfiniteScroll.RouteMixin, App.ResetableRouteMixin);
