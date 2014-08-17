3fb23c115038897d9906fc078d47b500be7bcdd0
Evan Owski <evan@heirloom.io>
2014-08-16 17:57:22 -0700

    Separates each photo grouping into its own controller/view.

---
+++ app/scripts/views/photoGroupingView.js
@@ -0,0 +1,46 @@
+App.PhotoGroupingView = Ember.View.extend({
+  classNames: ['photo-grouping'],
+
+  init: function () {
+    this.resizeSectionBorder = $.proxy(this.resizeSectionBorder, this);
+    this._super();
+  },
+
+  setupResizeHandler: function () {
+    $(window).on('resize', this.resizeSectionBorder);
+  }.on('didInsertElement'),
+
+  tearDownResizeHandler: function () {
+    $(window).off('resize', this.resizeSectionBorder);
+  }.on('willDestroyElement'),
+
+  resizeSectionBorder: function () {
+    Ember.run.scheduleOnce('afterRender', this, function () {
+      // in some test scenarios, this.$('.photo-grouping') is undefined
+      var $grouping = this.$();
+
+      if (!$grouping) {
+        return;
+      }
+
+      var $imgsCont = $grouping.find('.images-container');
+      var contOffset = $imgsCont.offset().left;
+      var outerRightEdge = $imgsCont.outerWidth() + contOffset;
+      var imgs = $imgsCont.children('img');
+      var rightMostImageRightEdge = 0;
+      var rightMostImageOuterRightEdge = 0;
+
+      for (var i = 0; i < imgs.size(); i++) {
+        var img = $(imgs.get(i));
+        rightMostImageRightEdge = img.offset().left + img.outerWidth();
+        rightMostImageOuterRightEdge = img.offset().left + img.outerWidth(true);
+
+        if (rightMostImageOuterRightEdge + img.outerWidth(true) > outerRightEdge) {
+          break;
+        }
+      }
+
+      $grouping.find('.border').width(rightMostImageRightEdge - contOffset);
+    });
+  }.observes('controller.photos.length')
+});
