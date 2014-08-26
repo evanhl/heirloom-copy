340f544a93dc10e98a5689a45531e67b11e26382
Evan Owski <evan@heirloom.io>
2014-08-26 14:34:25 -0700

    Fixes null object bug. Adds pointer cursor to photos.

--- app/scripts/controllers/photoController.js
+++ app/scripts/controllers/photoController.js
@@ -1,8 +1,8 @@
 //= require basePhotoController
 App.PhotoController = App.BasePhotoController.extend({
   needs: ['photoGroupings'],
-  photoGroupings: Ember.computed.alias('controllers.photoGroupings'),
-  photosModel: Ember.computed.alias('photoGroupings.compositeModel'),
+  photos: Ember.computed.alias('controllers.photoGroupings'),
+  photosModel: Ember.computed.alias('photos.compositeModel'),
 
   toPhoto: function (toId) {
     if (!toId) { return; }
