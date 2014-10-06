2cd6d0ac29d3a9afb3956b7923588de4dc113ecc
Evan Owski <evan@heirloom.net>
2014-10-06 13:29:24 -0700

    Adds WIP pre-launch home page with looping video. Adds pre-launch-specific SCSS file.

--- app/scripts/preLaunchApp.js
+++ app/scripts/preLaunchApp.js
@@ -1,5 +1,27 @@
 var App = Ember.Application.create({});
 
 App.ApplicationView = Ember.View.extend({
-  templateName: 'preLaunch'
+  templateName: 'preLaunch',
+
+  didInsertElement: function () {
+    this.$('a[href*=#]:not([href=#])').on('click', this.smoothScrollAnchorLinks);
+  },
+
+  willDestroyElement: function () {
+    this.$('a[href*=#]:not([href=#])').off('click', this.smoothScrollAnchorLinks);
+  },
+
+  // lifted from here: http://css-tricks.com/snippets/jquery/smooth-scrolling/
+  smoothScrollAnchorLinks: function () {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  }
 });
