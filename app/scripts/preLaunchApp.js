1f42e76d9f7aa817adaad90ef1738877bec4f9e9
Evan Owski <evan@heirloom.net>
2014-10-08 12:38:00 -0700

    Hook up formstack email form.

--- app/scripts/preLaunchApp.js
+++ app/scripts/preLaunchApp.js
@@ -1,19 +1,50 @@
-var App = Ember.Application.create({});
+window.App = Ember.Application.create({});
 
 App.ApplicationView = Ember.View.extend({
   templateName: 'preLaunch',
 
   didInsertElement: function () {
+    this.controller.set('disabled', false);
+    this.controller.set('notifyButtonText', "Notify Me");
     this.$('a[href*=#]:not([href=#])').on('click', this.smoothScrollAnchorLinks);
+    this.$('iframe').on('load', $.proxy(this.iframeOnload, this));
+    this.$('#email-form').on('submit', $.proxy(this.onSubmit, this));
+    this.$('.hp-text').on('keypress', $.proxy(this.onKeyPress, this));
+
   },
 
   willDestroyElement: function () {
-    this.$('a[href*=#]:not([href=#])').off('click', this.smoothScrollAnchorLinks);
+    this.$('a[href*=#]:not([href=#])').off();
+    this.$('iframe').off();
+    this.$('#email-form').off();
+  },
+
+  iframeOnload: function () {
+    if (!this.get('iframeOnloadIsElig')) { return; }
+
+    this.controller.set('notifyButtonText', "Sent");
+    this.controller.set('disabled', false);
+
+    this.$('.hp-text').val('');
+  },
+
+  onKeyPress: function (e) {
+    if (e.keyCode === 13) {
+      this.$('#email-form').submit();
+    }
+
+    this.controller.set('notifyButtonText', "Notify Me");
+  },
+
+  onSubmit: function () {
+    // some browsers fire an iframe onload willy nilly on page load
+    this.set('iframeOnloadIsElig', true);
+    this.controller.set('disabled', true);
   },
 
   // lifted from here: http://css-tricks.com/snippets/jquery/smooth-scrolling/
   smoothScrollAnchorLinks: function () {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
