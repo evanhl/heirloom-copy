12b808755b1b20f0c7d2e73c45681b97e86d75c5
Evan Owski <evan@heirloom.net>
2014-10-08 14:27:30 -0700

    Switch phone copy text to Brandon. Adds auto-focus on click "notify me".

--- app/scripts/preLaunchApp.js
+++ app/scripts/preLaunchApp.js
@@ -10,6 +10,7 @@ App.ApplicationView = Ember.View.extend({
     this.$('iframe').on('load', $.proxy(this.iframeOnload, this));
     this.$('#email-form').on('submit', $.proxy(this.onSubmit, this));
     this.$('.hp-text').on('keypress', $.proxy(this.onKeyPress, this));
+    this.$('.jump-to-notify').on('click', $.proxy(this.jumpToNotify, this));
 
   },
 
@@ -17,6 +18,7 @@ App.ApplicationView = Ember.View.extend({
     this.$('a[href*=#]:not([href=#])').off();
     this.$('iframe').off();
     this.$('#email-form').off();
+    this.$('.jump-to-notify').off();
   },
 
   iframeOnload: function () {
@@ -40,6 +42,14 @@ App.ApplicationView = Ember.View.extend({
     // some browsers fire an iframe onload willy nilly on page load
     this.set('iframeOnloadIsElig', true);
     this.controller.set('disabled', true);
+  },
+
+  jumpToNotify: function (e) {
+    var self = this;
+
+    setTimeout(function () {
+      self.$('.hp-text').focus();
+    }, 500);
   },
 
   // lifted from here: http://css-tricks.com/snippets/jquery/smooth-scrolling/
