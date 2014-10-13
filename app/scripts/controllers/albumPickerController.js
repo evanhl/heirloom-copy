00d842f38d984131a7044c0e47e0602e22e9357a
Evan Owski <evan@heirloom.net>
2014-10-13 14:38:08 -0700

    Modify album picker to use updated selection log. Fix bug where multiple albums can be selected.

--- app/scripts/controllers/albumPickerController.js
+++ app/scripts/controllers/albumPickerController.js
@@ -1,4 +1,4 @@
-App.AlbumPickerController = Ember.Controller.extend(Ember.Evented, {
+App.AlbumPickerController = Ember.Controller.extend(App.SelectableMixin, Ember.Evented, {
   needs: ['albums'],
   albums: Ember.computed.alias('controllers.albums'),
   loadingMore: Ember.computed.alias('albums.loadingMore'),
@@ -14,8 +14,10 @@ App.AlbumPickerController = Ember.Controller.extend(Ember.Evented, {
     this.get('albums').fetchPage(page, perPage);
   },
 
-  clearSelected: function () {
-    this.get('albums').setEach('selected', false);
+  // TODO: this is copy-pasted in a few places. consider including in SelectableMixin
+  deselect: function () {
+    this.trigger('deselect');
+    this.resetSelected();
   },
 
   actions: {
@@ -24,23 +26,27 @@ App.AlbumPickerController = Ember.Controller.extend(Ember.Evented, {
     },
 
     select: function (albumController) {
-      // TODO: move in to Album Controller when we get the chance
-      var album = albumController.get('model');
-      var newValue = album.toggleProperty('selected');
-      this.get('albums').setEach('selected', false);
-      album.set('selected', newValue);
+      var selected = albumController.get('selected');
+      var albumId = albumController.get('model.id');
+
+      this.deselect();
+      this.toggleSelected(albumId, selected);
+      albumController.set('selected', selected);
     },
 
     close: function () {
-      this.clearSelected();
+      this.resetSelected();
       this.send('closeModal');
     },
 
     // for now, this is only add to album, but there could
     // be other action types later on.
     complete: function () {
-      this.trigger('didSelect', this.get('albums').findBy('selected', true));
-      this.clearSelected();
+      var selectedAlbumId = parseInt(this.get('selectedIds')[0], 10);
+      var album = this.get('albums').findBy('id', selectedAlbumId);
+
+      this.trigger('didSelect', album);
+      this.resetSelected();
       this.send('closeModal');
     }
   }
