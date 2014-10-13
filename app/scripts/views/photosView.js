1163a291bc885b512c7388369b21d9c4b3b29783
Evan Owski <evan@heirloom.net>
2014-10-13 15:38:05 -0700

    Adds photo grid centering.

--- app/scripts/views/photosView.js
+++ app/scripts/views/photosView.js
@@ -1,8 +1,35 @@
 //= require ../utils/infiniteScroll
+
 App.PhotosView = Ember.View.extend(InfiniteScroll.ViewMixin, {
   scrollEl: 'body',
 
   clearSelected: function () {
     this.get('controller').resetSelected();
-  }.on('willDestroyElement')
+  }.on('willDestroyElement'),
+
+  setAutoWidth: function () {
+    Ember.run.schedule('afterRender', this, function () {
+      this.setContainerWidth();
+      this.proxiedSetContainerWidth = $.proxy(this.setContainerWidth, this);
+      $(window).on('resize', this.proxiedSetContainerWidth);
+    });
+  }.on('didInsertElement'),
+
+  unsetAutoWidth: function () {
+    $(window).off('resize', this.proxiedSetContainerWidth);
+  }.on('willDestroyElement'),
+
+  setContainerWidth: function () {
+    var $cont = this.$('.photos-container');
+    var contWidth, blockWidth, maxImgPerRow;
+
+    var $dummyPhoto = $('<div class="photo"></div>').hide();
+    blockWidth = $dummyPhoto.appendTo($('body')).outerWidth(true);
+    $dummyPhoto.remove();
+
+    $cont.css('width', 'auto');
+    contWidth = $cont.width();
+    maxImgPerRow = Math.floor(contWidth / blockWidth);
+    $cont.width(maxImgPerRow * blockWidth);
+  }
 });
