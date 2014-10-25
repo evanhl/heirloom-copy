e5c0942c3a33109effeaa2868baa7efc2bcf3a17
Jesse Atkinson <jatkinson@tunein.com>
2014-10-25 10:11:30 -0700

    Added in Question Index at top and scroll to animations. Scroll to works with javascript disabled as well. Clean up work to come. Tad messy JS right now.

--- app/scripts/faq.js
+++ app/scripts/faq.js
@@ -35,29 +35,97 @@
 
     cacheElements: function () {
       this.elms = {
-        $faq: $('#faq'),
-        $h2:  $('h2')
+        $bodyAndHtml: $('html, body'),
+        $faq:         $('#faq'),
+        $h2:          $('h2')
       };
     },
 
     formatHtml: function () {
-      var wrapAnswers;
+      var wrapAnswers, appendBackToTopLinks, questionTitleInfo, prependNameAnchorsToH2s, lis;
 
+      lis = '';
+
+      // Needed for accordian animation.
       wrapAnswers = function () {
         $(this).nextUntil('h2').wrapAll($('<div/>', { 'class': 'answer' }));
       };
 
-      this.elms.$h2.each(wrapAnswers);
+      prependNameAnchorsToH2s = function () {
+        var text  = $(this).text(),
+            name  = text.replace(/\s+/g, '-').toLowerCase(),
+            attrs = { 'class': 'title-anchors', data: {} };
+
+        attrs.name        = name;
+        attrs.data.title  = text;
+
+        $('<a/>', attrs).insertBefore($(this));
+      };
+
+      appendBackToTopLinks = function () {
+        $('.answer').append($('<a/>', { 'class': 'back-to-top', href: '#top', text: 'Back To Top' } ));
+      };
+
+      questionTitleInfo = function () {
+        this.elms.$titleAnchors = $('.title-anchors');
+
+        return this.elms.$titleAnchors.map(function () {
+          var linkInfo = {};
+
+          linkInfo.name  = $(this).attr('name');
+          linkInfo.title = $(this).data('title');
+
+          return linkInfo;
+        });
+      };
+
+      this.elms.$h2.each(function () {
+        wrapAnswers.call(this);
+        prependNameAnchorsToH2s.call(this);
+      });
+
+      appendBackToTopLinks();
+
+      $.each(questionTitleInfo.call(this), function () {
+        lis += '<li><a class="question" href="#' + this.name + '">' + this.title + '</a></li>';
+      });
+
+      // Creates the unorder list element to insert the question list into.
+      this.elms.$faq.prepend($('<ul/>', { 'class': 'questions' }).html(lis));
+      this.elms.$faq.prepend($('<a/>', { 'name': 'top' }));
+      this.elms.$question = $('.question');
     },
 
     bindEventListeners: function () {
-      var accordian;
+      var accordian, scrollToQuestion, scrollToTop;
 
       accordian = function (event) {
         $(event.target).next('.answer').slideToggle('fast');
       };
 
+      // NOTE: The deeplinking works without JavaScript, however if the user has JavaScript we're going to
+      // give them a fancy animation to scroll to it.
+      scrollToQuestion = function (event) {
+        event.preventDefault();
+
+        var self, name, $anchor, yPosition;
+
+        self      = event.data.namespace;
+        name      = $(event.target).attr('href').substring(1);
+        $anchor   = self.elms.$titleAnchors.filter(function () { return $(this).attr('name') === name; });
+        yPosition = Math.floor($anchor.offset().top);
+
+        self.elms.$bodyAndHtml.stop().animate({ scrollTop: yPosition }, 'fast');
+      };
+
+      scrollToTop = function (event) {
+        event.preventDefault();
+        event.data.namespace.elms.$bodyAndHtml.stop().animate({ scrollTop: 0 }, 'fast');
+      };
+
       this.elms.$h2.on('click', accordian);
+      this.elms.$question.on('click', { namespace: this }, scrollToQuestion);
+      $('.back-to-top').on('click', { namespace: this }, scrollToTop);
     }
   };
 
