5b555a3b9e4ff46da9c8fa504446ea0ff64059d0
Evan Owski <evan@heirloom.net>
2014-11-02 10:01:59 -0800

    Changes icon style and highlight color of groups list.

--- app/scripts/models/conversation.js
+++ app/scripts/models/conversation.js
@@ -1,7 +1,7 @@
 App.Conversation = Ember.Model.extend({
   id: Ember.attr(Number),
   name: Ember.attr(),
-  owner: Ember.attr(),
+  owner: Ember.belongsTo(App.Session, { key: 'owner', embedded: true }),
   is_owner: Ember.attr(Boolean),
   policy: Ember.attr(),
   member_count: 0
