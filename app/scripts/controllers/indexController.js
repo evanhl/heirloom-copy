b5d62ba0c15053638c3b9b87d4d9e9d66f90b895
Evan Owski <evan@heirloom.net>
2014-11-30 01:38:20 -0500

    Adds Zakynthos video to home page.

--- app/scripts/controllers/indexController.js
+++ app/scripts/controllers/indexController.js
@@ -2,10 +2,15 @@ App.IndexController = Ember.Controller.extend({
   actions: {
     hideVideo: function () {
       this.set('showVideo', false);
+      this.set('showStoryVideo', false);
     },
 
     showVideo: function () {
       this.set('showVideo', true);
+    },
+
+    showStoryVideo: function () {
+      this.set('showStoryVideo', true);
     }
   }
 });
