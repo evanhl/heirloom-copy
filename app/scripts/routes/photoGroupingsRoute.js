ba69df3425abd05c56c4edcb5410ac1f4a27d44c
Evan Owski <evan@heirloom.net>
2014-12-18 11:58:47 -0800

    Removes photo groupings.

--- app/scripts/routes/photoGroupingsRoute.js
+++
@@ -1,3 +0,0 @@
-// //= require signedInRouteMixin
-// //= require ../utils/infiniteScroll
-// App.PhotoGroupingsRoute = Ember.Route.extend(App.SignedInRouteMixin, InfiniteScroll.RouteMixin);
--- app/scripts/views/photoGroupingsView.js
+++
@@ -1,5 +0,0 @@
-// App.PhotoGroupingsView = Ember.View.extend({
-//   resetController: function () {
-//     this.controller.resetSelected();
-//   }.on('willDestroyElement')
-// });
