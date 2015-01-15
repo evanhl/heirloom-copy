2a1d2264d5e6c09c9570f7a2bcbef6e8b06763fc
Evan Owski <evan@heirloom.net>
2015-01-14 20:51:47 -0800

    Fixes https Zip file security issue.

--- app/scripts/config/production.js
+++ app/scripts/config/production.js
@@ -3,6 +3,5 @@ var HLConfig = {
   HOSTNAME: 'https://api.heirloom.net',
   canonicalDomain: 'https://www.heirloom.net',
   zipTemplateId: '56432f008fdf11e4b1f6bbd777c2f3d1',
-  zipHost: 'http://zip.heirloom.net/',
   dropboxAppKey: '9hrj55apfinev20'
 };
