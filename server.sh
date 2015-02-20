9a8474793d6f9c32bc2612f37687d3900c187c71
Evan Owski <evan@heirloom.net>
2015-02-19 17:08:42 -0800

    Short=circuits build process and runs node app.js directly.

--- server.sh
+++ server.sh
@@ -1,15 +1,6 @@
-# npm install -g grunt-cli; grunt build; node app.js
-
-if [ -d "./dist" ]; then
-  echo "Dist detected. Starting Node.js..."
-  node app.js
-else
-  echo "No dist detected. Building app..."
-  npm install -g grunt-cli && grunt build
-  if [ "$?" = "0" ]; then
-    echo "Starting Node.js..."
-    node app.js
-  else
-  	exit 1
-  fi
-fi
+echo "*******************"
+echo ""
+echo "Starting Node.js..."
+echo ""
+echo "*******************"
+node app.js
